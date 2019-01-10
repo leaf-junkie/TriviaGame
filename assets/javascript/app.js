814c78eb18e788b2208a1ac1837091129b44032c
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-10 18:30:53 -0500

    Created array of objects to hold questions and associated answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+// Trivia objects
+
+let coffeeFacts = [ 
+    {
+        question: 'Which country is the greatest producer of coffee?',
+        correctAnswer: 'Brazil',
+        wrongAnswer: 'Indonesia',
+        wrongAnswer: 'Costa Rica',
+        wrongAnswer: 'The U.S.'
+    }
+    {
+        question: 'What is an Americano?',
+        correctAnswer: 'Espresso diluted with water',
+        wrongAnswer: 'Espresso with a splash of cream',
+        wrongAnswer: 'Two shots of espresso',
+        wrongAnswer: 'Weak coffee'
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+    {
+        question: '',
+        correctAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: '',
+        wrongAnswer: ''
+    }
+]
