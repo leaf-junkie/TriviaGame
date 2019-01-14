81103e733b17b5c0928bb778733288e261f8e9d7
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-14 14:45:49 -0500

    delay timer, styling, adding code for images and messages between questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,79 +5,79 @@ $('document').ready(function() {
             question: 'Which country is the greatest producer of coffee?',
             answer: 'Brazil',
             options: ['Brazil', 'Indonesia', 'Costa Rica', 'The U.S.'],
-            img: 'https://insights.jumoreglobal.com/wp-content/uploads/2018/09/brazil-coffee.png'
+            image: 'https://insights.jumoreglobal.com/wp-content/uploads/2018/09/brazil-coffee.png'
         },
         {
             question: 'What is an Americano?',
             answer: 'Espresso diluted with water',
             options: ['Espresso diluted with water','Espresso with a splash of cream','Two shots of espresso','Weak coffee'],
-            img: 'https://www.simplyhealthyfamily.org/wp-content/uploads/2018/11/how-to-make-an-americano-coffee-3.jpg'
+            image: 'https://www.simplyhealthyfamily.org/wp-content/uploads/2018/11/how-to-make-an-americano-coffee-3.jpg'
         },
         {
             question: 'A cappuccino is an espresso-based drink. What is the ratio of its components?',
             answer: '1/3 espresso : 1/3 steamed milk : 1/3 foam',
             options: ['1/3 espresso : 1/3 steamed milk : 1/3 foam', '1/2 espresso : 1/4 steamed milk : 1/4 foam', '1/3 espresso : 1/3 water : 1/3 cream', '1/2 espresso : 1/2 foam'],
-            img: 'https://i.imgur.com/7BwsUK4.jpg?2'
+            image: 'https://i.imgur.com/7BwsUK4.jpg?2'
         },
         {
             question: 'What is a mocha?',
             answer: 'A latte flavored with chocolate',
             options: ['A latte flavored with chocolate', 'Hot chocolate', 'Coffee with a splash of chocolate milk', 'Chocolate flavored espresso'],
-            img: 'https://www.drippedcoffee.com/wp-content/uploads/2017/07/Mocha.jpg'
+            image: 'https://www.drippedcoffee.com/wp-content/uploads/2017/07/Mocha.jpg'
         },
         {
             question: 'What is an espresso macchiato?',
             answer: 'Espresso marked with a spot of foamed milk',
             options: ['Espresso marked with a spot of foamed milk', 'A single shot of espresso', 'A shot of espresso pulled for 30 seconds', 'A shot of espresso flavored with caramel'],
-            img: 'https://i.pinimg.com/originals/cd/84/70/cd84706c21e58d1c28d64222ab377077.jpg'
+            image: 'https://i.pinimg.com/originals/cd/84/70/cd84706c21e58d1c28d64222ab377077.jpg'
         },
         {
             question: 'What is a red eye?',
             answer: 'Drip coffee with a shot of espresso',
             options: ['Drip coffee with a shot of espresso', 'Drip coffee with two shots of espresso', 'two shots of espresso topped off with water', 'A latte made with decaf espresso'],
-            img: 'http://wkutalisman.com/wp-content/uploads/2016/09/redblack-1024x961.jpg'
+            image: 'http://wkutalisman.com/wp-content/uploads/2016/09/redblack-1024x961.jpg'
         },
         {
             question: 'How much caffeine is in coffee?',
             answer: 'About 95 mg per one 8 fl oz cup',
             options: ['About 95 mg per one 8 fl oz cup', '400 mg per 8 fl oz', 'Roughly 1 gram per cup', 'About 10 mg per 8 fl oz cup'],
-            img: 'http://www.ncausa.org/portals/56/Images/CoffeeHealth/coffee-caffeine-mitchell.png'
+            image: 'http://www.ncausa.org/portals/56/Images/CoffeeHealth/coffee-caffeine-mitchell.png'
         },
         {
             question: 'What is a key difference between light roasts and darker roasts?',
             answer: 'Light roasts contain more caffeine than dark roasts',
             options: ['Light roasts contain more caffeine than dark roasts', 'Light roasts are roasted in the sun, while dark roasts are roasted without light', 'The raw coffee beans are different colors', 'They are different varieties/subspecies'],
-            img: 'https://cdn-img.health.com/sites/default/files/styles/large_16_9/public/styles/main/public/gettyimages-151993684.jpg?itok=QejNB9K2'
+            image: 'https://cdn-img.health.com/sites/default/files/styles/large_16_9/public/styles/main/public/gettyimages-151993684.jpg?itok=QejNB9K2'
         },
         {
             question: 'What is the relationship between coffee and Coffea?',
             answer: 'Coffea is a genus of plants from which coffee beans originate',
             options: ['Coffea is a genus of plants from which coffee beans originate', 'Coffea refers to the flowers of the coffee plant', 'Coffee plants are grown in Africa, while Coffea plants are gown in South America', 'They are unrelated'],
-            img: 'https://cdn2.vectorstock.com/i/1000x1000/85/01/coffee-tree-vector-648501.jpg'
+            image: 'https://cdn2.vectorstock.com/i/1000x1000/85/01/coffee-tree-vector-648501.jpg'
         },
         {
             question: 'A coffee bean is actually...',
             answer: 'The seed of a fruit',
             options: ['The seed of a fruit', 'Just a bean', 'A nut similar to hazelnuts', 'A type of fruit'],
-            img: 'https://www.westrockcoffee.com/wp-content/uploads/2016/10/Anatomy-Of-A-Coffee-Bean-Blog-Image-V3-830x1024.jpg'
+            image: 'https://www.westrockcoffee.com/wp-content/uploads/2016/10/Anatomy-Of-A-Coffee-Bean-Blog-Image-V3-830x1024.jpg'
         },
         {
             question: 'Why is coffee roasted?',
             answer: 'To provide them with flavor and aroma',
             options: ['To provide them with flavor and aroma', 'To activate the caffeine', 'To make them safe to consume', 'To burn off the excess caffeine'],
-            img: 'https://camanoislandcoffee.com/wp-content/uploads/2015/12/Art-of-Roasting-Coffee.jpg'
+            image: 'https://camanoislandcoffee.com/wp-content/uploads/2015/12/Art-of-Roasting-Coffee.jpg'
         },
         {
             question: 'What is an affogato?',
             answer: 'A scoop of vanilla ice cream topped with a shot of espresso',
             options: ['A scoop of vanilla ice cream or gelato topped with a shot of espresso', 'A cappuccino over ice', 'A milkshake made with chocolate syrup, espresso, and gelato', 'Coffee-flavored gelato'],
-            img: 'http://www.the350degreeoven.com/wp-content/uploads/2015/08/DSC_0173-copy.jpg'
+            image: 'http://www.the350degreeoven.com/wp-content/uploads/2015/08/DSC_0173-copy.jpg'
         },
         {
             question: 'What is the difference between a cappuccino and a latte?',
             answer: 'Cappuccinos have much more foam, while lattes are milkier',
             options: ['Cappuccinos have much more foam, while lattes are milkier', 'Nothing - both names refer to the same drink', 'A cappuccino is made with cream, while a latte is made with milk', 'A latte is topped with foam, while a cappuccino is not'],
-            img: 'https://i.pinimg.com/originals/cb/78/ea/cb78ea22aae7f98ca1bc73f28f6471ca.jpg'
+            image: 'https://i.pinimg.com/originals/cb/78/ea/cb78ea22aae7f98ca1bc73f28f6471ca.jpg'
         },
     ];
 
@@ -87,7 +87,8 @@ $('document').ready(function() {
     let numOfWrongGuesses = 0;
     let numOfTimesRunOut = 0;
 
-    const questionTime = 5;
+    const questionTime = 15;
+    const delay = 4;
 
     // G A M E P L A Y
     // When Play button is clicked, game loads
@@ -100,14 +101,26 @@ $('document').ready(function() {
         // Check if answer is correct
         if (userChoice === correctAnswer) {
             numOfRightGuesses++;
-            $('right').css({visibility: 'visible'}); //TODO: create div where this will be shown
             questionIndex++;
+            
+            // Delay question here
+            setTimeout(function() {
+                // Display relevant image and message
+                $('#rightAnswer').css({visibility: 'visible'});
+                $('#objImage').triviaObjects[questionIndex].image;
+            }, delay * 1000);
             nextQuestion();
         } else {
             numOfWrongGuesses++;
-            $('wrong').css({visibility: 'visible'}); //TODO: create div where this will be shown
-            $('showCorrect').text(correctAnswer);
             questionIndex++;
+            
+            // Delay question here
+            setTimeout(function(){
+                // Display relevant image and message
+                $('#wrongAnswer').css({visibility: 'visible'});
+                // Display correct answer
+                $('#showCorrect').text(correctAnswer);
+            }, delay * 1000);  
             nextQuestion();
         }            
         
@@ -159,21 +172,31 @@ $('document').ready(function() {
     // If game is over, show scores and option to restart game (without reloading page)
     function gameOver() {
         //  Show scores
-        $('.game-over-screen').css({visibility: 'visible'});
-        $('.game-over-screen-right').text('Right: ' + numOfRightGuesses);
-        $('.game-over-screen-wrong').text('Wrong: ' + numOfWrongGuesses);
-        $('.game-over-screen-timeout').text('Ran out of time: ' + numOfTimesRunOut);   
+        $('#gameOverPosition').css({visibility: 'visible'});
+        $('#gameOverScreen').css({visibility: 'visible'});
+        $('#right').text('Number of Questions Right: ' + numOfRightGuesses);
+        $('#wrong').text('Number of Questions Wrong: ' + numOfWrongGuesses);
+        $('#timeRanOut').text('Times you ran out of time: ' + numOfTimesRunOut);   
         console.log('Youve answered all of the questions. Lets see how you did!');
+        
+        resetGame();
     }
 
     // If player clicks "play again" button, reset values
     function resetGame() {
         $('#playButton').click(function() {
-            let arrayOfAskedQuestions = [''];
             let questionIndex = 0;
             let numOfRightGuesses = 0;
             let numOfWrongGuesses = 0;
             let numOfTimesRunOut = 0;
         });
     }
+
+    function changeVisibility() {
+        if (visibility != hidden) {
+            $('#gameOverPosition').css({visibility: 'hidden'});
+            $('#gameOverScreen').css({visibility: 'hidden'});
+        }
+    }
+    
 }); // End ready
