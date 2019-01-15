6c74ed506a893a411de0c73510e9cc4d89d974b8
leaf-junkie <ragsdale.jar@gmail.com>
2019-01-15 02:04:55 -0500

    changed all images to have square aspect ratio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,31 +6,31 @@ $('document').ready(function() {
             question: 'Which country is the greatest producer of coffee?',
             answer: 'Brazil',
             options: ['Brazil', 'Indonesia', 'Costa Rica', 'The U.S.'],
-            image: 'https://insights.jumoreglobal.com/wp-content/uploads/2018/09/brazil-coffee.png'
+            image: 'https://muddydogcoffee.com/wp-content/uploads/2017/03/brazilian-coffee-berries-being-harvested.-source-viralgecko.com_-600x600.jpg'
         },
         {
             question: 'What is an Americano?',
             answer: 'Espresso diluted with water',
             options: ['Espresso diluted with water','Espresso with a splash of cream','Two shots of espresso','Weak coffee'],
-            image: 'https://www.simplyhealthyfamily.org/wp-content/uploads/2018/11/how-to-make-an-americano-coffee-3.jpg'
+            image: 'https://i.pinimg.com/originals/f0/b8/8f/f0b88ffc82c1127b4e56b7952791f5e5.jpg'
         },
         {
             question: 'A cappuccino is an espresso-based drink. What is the ratio of its components?',
             answer: '1/3 espresso : 1/3 steamed milk : 1/3 foam',
             options: ['1/3 espresso : 1/3 steamed milk : 1/3 foam', '1/2 espresso : 1/4 steamed milk : 1/4 foam', '1/3 espresso : 1/3 water : 1/3 cream', '1/2 espresso : 1/2 foam'],
-            image: 'https://i.imgur.com/7BwsUK4.jpg?2'
+            image: 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500'
         },
         {
             question: 'What is a mocha?',
             answer: 'A latte flavored with chocolate',
             options: ['A latte flavored with chocolate', 'Hot chocolate', 'Coffee with a splash of chocolate milk', 'Chocolate flavored espresso'],
-            image: 'https://www.drippedcoffee.com/wp-content/uploads/2017/07/Mocha.jpg'
+            image: 'https://i.pinimg.com/736x/f3/d2/38/f3d2381dc39d73a8c40f94476cef3906--starbucks-christmas-christmas-drinks.jpg'
         },
         {
             question: 'What is an espresso macchiato?',
             answer: 'Espresso marked with a spot of foamed milk',
             options: ['Espresso marked with a spot of foamed milk', 'A single shot of espresso', 'A shot of espresso pulled for 30 seconds', 'A shot of espresso flavored with caramel'],
-            image: 'https://i.pinimg.com/originals/cd/84/70/cd84706c21e58d1c28d64222ab377077.jpg'
+            image: 'https://s3-media1.fl.yelpcdn.com/bphoto/fzCW3H_VW2tKFwgxyYfJVg/ls.jpg'
         },
         {
             question: 'What is a red eye?',
@@ -42,43 +42,43 @@ $('document').ready(function() {
             question: 'How much caffeine is in coffee?',
             answer: 'About 95 mg per one 8 fl oz cup',
             options: ['About 95 mg per one 8 fl oz cup', '400 mg per 8 fl oz', 'Roughly 1 gram per cup', 'About 10 mg per 8 fl oz cup'],
-            image: 'http://www.ncausa.org/portals/56/Images/CoffeeHealth/coffee-caffeine-mitchell.png'
+            image: 'https://stefanprice.files.wordpress.com/2012/09/cafinev24.jpg'
         },
         {
             question: 'What is a key difference between light roasts and darker roasts?',
             answer: 'Light roasts contain more caffeine than dark roasts',
             options: ['Light roasts contain more caffeine than dark roasts', 'Light roasts are roasted in the sun, while dark roasts are roasted without light', 'The raw coffee beans are different colors', 'They are different varieties/subspecies'],
-            image: 'https://cdn-img.health.com/sites/default/files/styles/large_16_9/public/styles/main/public/gettyimages-151993684.jpg?itok=QejNB9K2'
+            image: 'https://s-media-cache-ak0.pinimg.com/originals/e0/e4/a8/e0e4a815823e147d70b70ef8d6ed878a.jpg'
         },
         {
             question: 'What is the relationship between coffee and Coffea?',
             answer: 'Coffea is a genus of plants from which coffee beans originate',
             options: ['Coffea is a genus of plants from which coffee beans originate', 'Coffea refers to the flowers of the coffee plant', 'Coffee plants are grown in Africa, while Coffea plants are gown in South America', 'They are unrelated'],
-            image: 'https://cdn2.vectorstock.com/i/1000x1000/85/01/coffee-tree-vector-648501.jpg'
+            image: 'https://i.ebayimg.com/images/g/rwoAAOSwKQ9Z9YwF/s-l300.jpg'
         },
         {
             question: 'A coffee bean is actually...',
             answer: 'The seed of a fruit',
             options: ['The seed of a fruit', 'Just a bean', 'A nut similar to hazelnuts', 'A type of fruit'],
-            image: 'https://www.westrockcoffee.com/wp-content/uploads/2016/10/Anatomy-Of-A-Coffee-Bean-Blog-Image-V3-830x1024.jpg'
+            image: 'https://i.pinimg.com/originals/d3/14/fb/d314fb94d221cc814e950295c68c6129.jpg'
         },
         {
             question: 'Why is coffee roasted?',
             answer: 'To provide them with flavor and aroma',
             options: ['To provide them with flavor and aroma', 'To activate the caffeine', 'To make them safe to consume', 'To burn off the excess caffeine'],
-            image: 'https://camanoislandcoffee.com/wp-content/uploads/2015/12/Art-of-Roasting-Coffee.jpg'
+            image: 'https://driftaway.coffee/wp-content/uploads/2015/08/shutterstock_182457260-min.jpg'
         },
         {
             question: 'What is an affogato?',
             answer: 'A scoop of vanilla ice cream topped with a shot of espresso',
             options: ['A scoop of vanilla ice cream or gelato topped with a shot of espresso', 'A cappuccino over ice', 'A milkshake made with chocolate syrup, espresso, and gelato', 'Coffee-flavored gelato'],
-            image: 'http://www.the350degreeoven.com/wp-content/uploads/2015/08/DSC_0173-copy.jpg'
+            image: 'https://photo.foodgawker.com/wp-content/uploads/2018/02/3169123.jpg'
         },
         {
             question: 'What is the difference between a cappuccino and a latte?',
             answer: 'Cappuccinos have much more foam, while lattes are milkier',
             options: ['Cappuccinos have much more foam, while lattes are milkier', 'Nothing - both names refer to the same drink', 'A cappuccino is made with cream, while a latte is made with milk', 'A latte is topped with foam, while a cappuccino is not'],
-            image: 'https://i.pinimg.com/originals/cb/78/ea/cb78ea22aae7f98ca1bc73f28f6471ca.jpg'
+            image: 'https://i.pinimg.com/originals/59/68/e5/5968e59784c25a9d312dba1adddf5200.jpg'
         },
     ];
 
